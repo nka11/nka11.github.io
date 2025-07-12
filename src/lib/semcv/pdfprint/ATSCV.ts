324cd5c4ce5e5b6e917584e178a24859bcec2a18
Nicodev <nicodev@deepthought-solutions.com>
2025-07-12 11:13:18 +0200

    Generateur CV ATS pre-version

--- src/lib/semcv/pdfprint/ATSCV.ts
+++ src/lib/semcv/pdfprint/ATSCV.ts
@@ -1,12 +1,13 @@
 import { jsPDF } from "jspdf";
 import type { NamedNode } from "oxigraph";
-import type { ICredentialDetails, IOrganizationRole, IPersonDetails, IProjectDetail } from "../models";
+import type { ICredentialDetails, IEducationDetails, IOrganizationRole, IPersonDetails, IProjectDetail } from "../models";
 import { listOrgRoles } from "../adapters/experienceAdapter";
 import { skillsCounts } from "../adapters/skillsAdapter";
 import { compareExperience } from "../semantic_cv_store";
 import { listCredentials } from "../adapters/credentialsAdapter";
 import { extractYear, formatDateFr } from "$lib/dateFormatter";
 import { listProjects } from "../adapters/projectsAdapter";
+import { listEducations } from "../adapters/educationAdapters";
 
 interface DocState {
   doc: jsPDF,
@@ -30,45 +31,43 @@ function writeBlock(state:DocState, text: string, blockMargin: number, lineHeigh
 }
 
 
-function printProjectDetails(state:DocState, project:IProjectDetail, lang: string) {
+function printProjectDetails(state:DocState, project:IProjectDetail, lang: string, showDates=false) {
   let headerline = "";
-  // if (project.projectStartDate || project.projectEndDate) {
-  //   if (project.projectStartDate && project.projectEndDate) {
-  //     headerline += "de ";
-  //   }
-  //   if (project.projectStartDate && !project.projectEndDate) {
-  //     headerline += "depuis ";
-  //   }
-  //   if (project.projectStartDate) {
-  //     headerline += formatDateFr(project.projectStartDate.value);
-  //   }
-  //   if (project.projectStartDate && project.projectEndDate) {
-  //     headerline += " à ";
-  //   }
-  //   if (project.projectEndDate) {
-  //     headerline += formatDateFr(project.projectEndDate.value);
-  //   }
-  // }
+  if (showDates) {
+    if (project.projectStartDate || project.projectEndDate) {
+      if (project.projectStartDate && project.projectEndDate) {
+        headerline += "de ";
+      }
+      if (project.projectStartDate && !project.projectEndDate) {
+        headerline += "depuis ";
+      }
+      if (project.projectStartDate) {
+        headerline += formatDateFr(project.projectStartDate.value);
+      }
+      if (project.projectStartDate && project.projectEndDate) {
+        headerline += " à ";
+      }
+      if (project.projectEndDate) {
+        headerline += formatDateFr(project.projectEndDate.value);
+      }
+    }
+  }
   headerline += "- " + project.projectName?.value;
 
   // state.y +=  2;
   state.doc.setFontSize(11);
   state.doc.setFont("helvetica", "bold");
   
-  writeBlock(state,headerline,4,5)
-  state.doc.setFont("helvetica", "normal");
-  state.doc.setFontSize(10);
-  
+  writeBlock(state,headerline,4,5);
   
   (project.projectDescription?.value as string + "").split('\n').forEach((line: string) => {
           
           line = line.trim();
           if (line == "") return;
+          
+          state.doc.setFont("helvetica", "normal");
+          state.doc.setFontSize(10);
           writeBlock(state,line,8,4);
-          // const lines = state.doc.splitTextToSize(line,state.maxWidth);
-          // // console.log(lines);
-          // state.doc.text(lines, state.margin + 4,state.y);
-          // state.y +=  (lines.length) * 4;
         })
 }
 
@@ -103,6 +102,42 @@ function printRoleCredentialDetails(state:DocState, role:IOrganizationRole, cred
   state.doc.setFont("helvetica", "normal");
   state.doc.setFontSize(10);
   const projectsDetails = listProjects(credential.credential, "schema:subjectOf", lang);
+  projectsDetails.forEach((project) => {
+    printProjectDetails(state,project, lang);
+  });
+}
+
+function printEducationDetails(state:DocState, education: IEducationDetails, lang:string) {
+  let headerline = "";
+  if (education.educStart || education.educEnd) {
+    if (education.educStart && education.educEnd) {
+      headerline += "de ";
+    }
+    if (education.educStart && !education.educEnd) {
+      headerline += "depuis ";
+    }
+    if (education.educStart) {
+      headerline += formatDateFr(education.educStart.value);
+    }
+    if (education.educStart && education.educEnd) {
+      headerline += " à ";
+    }
+    if (education.educEnd) {
+      headerline += formatDateFr(education.educEnd.value);
+    }
+  }
+  headerline += " - " + education.educLevel?.value;
+  if (education.educLocationName) {
+    headerline += " - " + education.educLocationName?.value;
+  }
+  state.y +=  3;
+  state.doc.setFontSize(12);
+  state.doc.setFont("helvetica", "bold");
+  writeBlock(state,headerline,0,6)
+  // state.doc.text(headerline, state.margin, state.y);
+  state.doc.setFont("helvetica", "normal");
+  state.doc.setFontSize(10);
+  const projectsDetails = listProjects(education.educ, "schema:subjectOf", lang);
   projectsDetails.forEach((project) => {
     printProjectDetails(state,project, lang);
   });
@@ -150,55 +185,10 @@ export function generateATS_CV(personDetails: IPersonDetails, lang: string): jsP
  
   let skills = skillsCounts(lang);
   let organizationRoles = listOrgRoles(personDetails?.person, "schema:hasOccupation",lang).sort(compareExperience);
+  let educationList = listEducations(personDetails.person,"schema:alumniOf",lang);
   
   organizationRoles.forEach((role) => {
-   
-    let headerline = "";
-    if (role.description) {
-        if (role.startDate || role.endDate) {
-          if (role.startDate && role.endDate) {
-            headerline += "de ";
-          }
-          if (role.startDate && !role.endDate) {
-            headerline += "depuis ";
-          }
-          if (role.startDate) {
-            headerline += formatDateFr(role.startDate.value);
-          }
-          if (role.startDate && role.endDate) {
-            headerline += " à ";
-          }
-          if (role.endDate) {
-            headerline += formatDateFr(role.endDate.value);;
-          }
-        }
-
-       headerline += " -  " + (role.roleName?.value ? role.roleName?.value : "") ;
-        if (role.employer) {
-          headerline += " - " + role.employer?.value
-        }
-        doc.setFont("helvetica", "bold");
-        doc.setFontSize(12);
-        writeBlock(state,headerline,0,6)
-        
-        // doc.text(headerline, margin,state.y);  
-        
-        doc.setFont("helvetica", "normal");
-        (role.description?.value as string + "").split('\n').forEach((line: string) => {
-          
-          // state.y +=  6;
-          line = line.trim();
-          writeBlock(state,line,5,5)
-          // const lines = doc.splitTextToSize(line,maxWidth);
-          // doc.text(lines, margin + 4,state.y);
-          // state.y +=  (lines.length -1) * 5;
-          
-        })
-    }
-    const credentialsDetails = listCredentials(role.role, "schema:hasCredential", lang);
-    credentialsDetails.forEach((credential) => {
-      printRoleCredentialDetails(state, role, credential, lang);
-    })
+    printOrganizationRole(role, doc, state, lang);
   });
 
   // Section : Formation
@@ -206,21 +196,67 @@ export function generateATS_CV(personDetails: IPersonDetails, lang: string): jsP
   doc.setFontSize(16);
   doc.setFont("helvetica", "bold");
   doc.text("Formation", margin,state.y);
-
   state.y +=  8;
-  doc.setFontSize(12);
-  doc.setFont("helvetica", "normal");
-  doc.text("Master Informatique - Université de Paris (2018 - 2020)", margin,state.y);
+  educationList.forEach((education) => {
+    printEducationDetails(state,education,lang);
+  })
 
   // Section : Compétences
   state.y +=  10;
   doc.setFontSize(16);
   doc.setFont("helvetica", "bold");
   doc.text("Compétences", margin,state.y);
-
+  let skillsList = skills.map((skill) => skill.skillName.value).join(', ');
   state.y +=  8;
-  doc.setFontSize(12);
+  doc.setFontSize(10);
   doc.setFont("helvetica", "normal");
-  doc.text("JavaScript, TypeScript, React, Node.js, SQL, Git, Docker", margin,state.y);
+  writeBlock(state,skillsList,0,5);
   return doc;
+}
+
+function printOrganizationRole(role: IOrganizationRole, doc: jsPDF, state: { doc: jsPDF; y: number; margin: number; maxWidth: number; }, lang: string) {
+  let headerline = "";
+  if (role.description) {
+    if (role.startDate || role.endDate) {
+      if (role.startDate && role.endDate) {
+        headerline += "de ";
+      }
+      if (role.startDate && !role.endDate) {
+        headerline += "depuis ";
+      }
+      if (role.startDate) {
+        headerline += formatDateFr(role.startDate.value);
+      }
+      if (role.startDate && role.endDate) {
+        headerline += " à ";
+      }
+      if (role.endDate) {
+        headerline += formatDateFr(role.endDate.value);;
+      }
+    }
+
+    headerline += " -  " + (role.roleName?.value ? role.roleName?.value : "");
+    if (role.employer) {
+      headerline += " - " + role.employer?.value;
+    }
+    doc.setFont("helvetica", "bold");
+    doc.setFontSize(12);
+    writeBlock(state, headerline, 0, 6);
+
+    // doc.text(headerline, margin,state.y);  
+    doc.setFont("helvetica", "normal");
+    (role.description?.value as string + "").split('\n').forEach((line: string) => {
+
+      // state.y +=  6;
+      line = line.trim();
+      writeBlock(state, line, 5, 5);
+      // const lines = doc.splitTextToSize(line,maxWidth);
+      // doc.text(lines, margin + 4,state.y);
+      // state.y +=  (lines.length -1) * 5;
+    });
+  }
+  const credentialsDetails = listCredentials(role.role, "schema:hasCredential", lang);
+  credentialsDetails.forEach((credential) => {
+    printRoleCredentialDetails(state, role, credential, lang);
+  });
 }
