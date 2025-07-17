dbae412a364a251a3a0dac15afb8a3cac3a1eaf6
Nicodev <nicodev@deepthought-solutions.com>
2025-07-17 23:22:51 +0200

    Work on custom theme

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defaultTheme } from '@sveltepress/theme-default'
 import { sveltepress } from '@sveltepress/vite'
 
 import path from 'path'
+import type { BundledLanguage } from 'shiki/langs'
 import Unocss from 'unocss/vite'
 
 import { defineConfig } from 'vite'
@@ -9,37 +10,43 @@ import { defineConfig } from 'vite'
 
 const config = defineConfig({
 	plugins: [
-		Unocss(),
+		// ,
 		sveltepress({
-			// theme: {
-			// 	globalLayout: path.resolve(__dirname, 'src/theme/GlobalLayout.svelte'),
-			// },
-			theme: defaultTheme({
-				//globalLayout: path.resolve(__dirname, 'src/theme/GlobalLayout.svelte'),
-				navbar: [
-					{
-						title: "Articles",
-						to: "/articles"
-					}
-					// Add your navbar configs here
-				],
-				sidebar: {
-					
-					// Add your sidebar configs here
-				},
-				github: 'https://github.com/nka11',
-				logo: "/nkaLogo-simple.svg",
-				//logo: '/sveltepress.svg',
-				themeColor: {
-					gradient: {
-						start: '#444444',
-						end: '#555555'
-
-					},
-					dark: '#222222',
-					light: '#ffffff'
+			theme: {
+				globalLayout: path.resolve(__dirname, 'src/theme/GlobalLayout.svelte'),
+				name: '',
+				pageLayout: '',
+				vitePlugins: [Unocss()],
+				highlighter: function (code: string, lang: BundledLanguage, meta?: string): string | Promise<string> {
+					throw new Error('Function not implemented.')
 				}
-			}),
+			},
+			// theme: defaultTheme({
+			// 	//globalLayout: path.resolve(__dirname, 'src/theme/GlobalLayout.svelte'),
+			// 	navbar: [
+			// 		{
+			// 			title: "Articles",
+			// 			to: "/articles"
+			// 		}
+			// 		// Add your navbar configs here
+			// 	],
+			// 	sidebar: {
+					
+			// 		// Add your sidebar configs here
+			// 	},
+			// 	github: 'https://github.com/nka11',
+			// 	logo: "/nkaLogo-simple.svg",
+			// 	//logo: '/sveltepress.svg',
+			// 	themeColor: {
+			// 		gradient: {
+			// 			start: '#444444',
+			// 			end: '#555555'
+
+			// 		},
+			// 		dark: '#222222',
+			// 		light: '#ffffff'
+			// 	}
+			// }),
 			siteConfig: {
 				
 				title: 'Nicolas Karageuzian',
