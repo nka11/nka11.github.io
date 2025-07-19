b6d62bb472f2894b7a8f949be3748e44bf915e92
Nicodev <nicodev@deepthought-solutions.com>
2025-07-20 01:24:10 +0200

    Some cleaning

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,4 @@
 // This help your get the type tips for sveltepress virtual modules
-/// <reference types="@sveltepress/theme-default/types" />
 /// <reference types="@sveltepress/vite/types" />
 
 // See https://kit.svelte.dev/docs/types#app
