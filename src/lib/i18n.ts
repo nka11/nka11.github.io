e130ddcc2b168c98f422356477241cdc5d9018af
Nicodev <nka11@users.noreply.github.com>
2025-08-14 19:05:51 +0200

    Fix ts code for locales

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,7 +1,7 @@
-import { dictionary, locale as svelteLocale, _ } from 'svelte-i18n';
+import { unwrapFunctionStore, dictionary, locale as svelteLocale, format as _ } from 'svelte-i18n';
 import { derived } from 'svelte/store';
 import { browsingPreferences } from '$lib/state.svelte';
-
+const $_ = unwrapFunctionStore(_)
 function setupI18n() {
   dictionary.set({
     en: {
@@ -15,6 +15,6 @@ function setupI18n() {
   svelteLocale.set(browsingPreferences.lang);
 }
 
-const T = derived(svelteLocale, $locale => (key, ...args) => _().t(key, ...args));
+const T = derived(svelteLocale, $locale => (key, ...args) => $_(key, ...args));
 
 export { T, setupI18n, svelteLocale as locale };
